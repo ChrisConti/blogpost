1dc3fad6eea801090c82e9717a4d78e6419c7bbf
Chris <christoconti@hotmail.fr>
2018-10-23 21:39:56 +0200

    delete old client

--- client2/src/actions/type.js
+++
@@ -1,5 +0,0 @@
-export const FETCH_USER = "fetch_user";
-export const LOG_OUT_USER = "log_out_user";
-export const FETCH_TRIPS = "fetch_trips";
-export const ADD_TRIP = "add_trip";
-export const DELETE_TRIP = "delete_trip";
